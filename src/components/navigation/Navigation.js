3ee04dfa5c4cde90073ecd26e96d561a7e968444
KevinMB22 <73042033+KevinMB22@users.noreply.github.com>
2022-10-25 17:58:21 +0200

    Update Navigation.js

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,6 +3,7 @@ import './navigation.scss';
 import Logo from '../../assets/TakeAll.svg';
 import Hamburger from '../../assets/hamburger.svg'
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 function Navigation(){
     return(
@@ -15,10 +16,10 @@ function Navigation(){
                 <img src={Logo} />                
             </figure>
             <figure className="bar">
-               <Link className="navBtn" to={'/'}><h3>ACCUEIL</h3> </Link>
-               <Link className="navBtn" to={'#'}><h3>COMMENT SA MARCHE ?</h3></Link>
-                <Link className="navBtn" to={'/aide'}><h3>SITES COMPATIBLES</h3></Link>
-                <Link className="navBtn" to={'/aide'}><h3>AIDE</h3></Link>
+               <Link className="navBtn" to={'/'}><motion.h3 initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{delay: 0.7}}>ACCUEIL</motion.h3></Link>
+               <Link className="navBtn" to={'#'}><motion.h3 initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{delay: 0.9}} >COMMENT SA MARCHE ?</motion.h3></Link>
+                <Link className="navBtn" to={'/aide'}><motion.h3 initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{delay: 1.1}}>SITES COMPATIBLES</motion.h3></Link>
+                <Link className="navBtn" to={'/aide'}><motion.h3 initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{delay: 1.3}}>AIDE</motion.h3></Link>
             </figure>
         </nav>
     )
